f4dfe879bc2d4a0f6487ac450f221828d573175c
AnastasiaZAYU <anastasiya7.0312@gmail.com>
2023-05-28 13:27:28 +0300

    Transfer files to GitHub

---
+++ R_Tree/R_Tree/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R_Tree
+{
+    class Database
+    {
+        List<string> name;
+        List<RTree> address;
+
+        public Database()
+        {
+            name = new List<string>();
+            address = new List<RTree>();
+        }
+
+        public void Add(string str, RTree link)
+        {
+            name.Add(str);
+            address.Add(link);
+        }
+
+        public int Contains(string str)
+        {
+            if (name.Contains(str)) return 1;
+            return 0;
+        }
+
+        public RTree Link(string str)
+        {
+            int index = name.IndexOf(str);
+            return address[index];
+        }
+    }
+}
---
+++ R_Tree/R_Tree/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R_Tree
+{
+    public class Node
+    {
+        public Node left, right;
+        public Rectangle rec;
+        public Point point;
+
+        public Node(int x0, int x1, int y0, int y1)
+        {
+            left = null;
+            right = null;
+            rec = new Rectangle(x0, x1, y0, y1);
+            point = null;
+        }
+
+        public Node(int x, int y)
+        {
+            left = null;
+            right = null;
+            rec = null;
+            point = new Point(x, y);
+        }
+    }
+}
