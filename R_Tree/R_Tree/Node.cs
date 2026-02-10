4b0fe76becce070c6c9d87baffd01e5536065caf
AnastasiaZAYU <anastasiya7.0312@gmail.com>
2026-02-10 10:47:16 +0200

    refactor: modernize Node class with MBR management

--- R_Tree/R_Tree/Node.cs
+++ R_Tree/R_Tree/Node.cs
@@ -8,24 +8,43 @@ namespace R_Tree
 {
     public class Node
     {
-        public Node left, right;
-        public Rectangle rec;
-        public Point point;
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public Rectangle MBR { get; set; } // Minimum Bounding Rectangle
+        public Point DataPoint { get; set; }
 
-        public Node(int x0, int x1, int y0, int y1)
-        {
-            left = null;
-            right = null;
-            rec = new Rectangle(x0, x1, y0, y1);
-            point = null;
-        }
+        public bool IsLeaf => Left == null;
 
+        // Leaf constructor
         public Node(int x, int y)
         {
-            left = null;
-            right = null;
-            rec = null;
-            point = new Point(x, y);
+            DataPoint = new Point(x, y);
+            MBR = new Rectangle(x, x, y, y);
+        }
+
+        // Constructor for internal node
+        public Node(Node left, Node right)
+        {
+            Left = left;
+            Right = right;
+            UpdateMBR();
+        }
+
+        public void UpdateMBR()
+        {
+            if (Left != null && Right != null)
+            {
+                MBR = new Rectangle(
+                    Math.Min(Left.MBR.XMin, Right.MBR.XMin),
+                    Math.Max(Left.MBR.XMax, Right.MBR.XMax),
+                    Math.Min(Left.MBR.YMin, Right.MBR.YMin),
+                    Math.Max(Left.MBR.YMax, Right.MBR.YMax)
+                    );
+            }
+            else if (Left != null)
+            {
+                MBR = Left.MBR;
+            }
         }
     }
 }
